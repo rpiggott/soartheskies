76fbee7cbeca23c5e9841b3d094b3680892b90a7
rpiggott <47783776+rpiggott@users.noreply.github.com>
2019-03-04 11:19:48 -0500

    Testing PDO

--- module/About/src/Service/PageAboutMetaKeywordLinkRepository.php
+++ module/About/src/Service/PageAboutMetaKeywordLinkRepository.php
@@ -17,13 +17,23 @@ class PageAboutMetaKeywordLinkRepository implements PageAboutMetaKeywordLinkRepo
 
     public function findKeywordList(): PageAboutMetaKeywordLink
     {
-        $repository = $this->entityManager->getRepository(PageAboutMetaKeywordLink::class);
+#        $repository = $this->entityManager->getRepository(PageAboutMetaKeywordLink::class);
+#
+#        $qb = $repository->createQueryBuilder('l');
+#                      $qb->join('\Application\Entity\MetaKeywords' , 'k', 'WITH', 'k.reference = l.metaKeywordReference');
+#                      $qb->expr()->isNull('l.removeDate');
+#
+#        return $qb->getQuery()->getSingleResult();
 
-        $qb = $repository->createQueryBuilder('l');
-                      $qb->join('\Application\Entity\MetaKeywords' , 'k', 'WITH', 'k.reference = l.metaKeywordReference');
-                      $qb->expr()->isNull('l.removeDate');
+        $sql = "SELECT * FROM page_about_meta_keyword_link INNER JOIN meta_keywords USING (reference);";
 
-        return $qb->getQuery()->getSingleResult();
+        $stmt = $this->entityManager->getConnection()->prepare($sql);
+        $result = $stmt->execute();
+
+        foreach ($stmt as $row) {
+            print_r( $row );
+        }
+
     }
 }
 
